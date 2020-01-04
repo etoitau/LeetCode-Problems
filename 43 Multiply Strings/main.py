4f3c5d76273fa42a5882fbd16557ba35c63b07db
Kyle Chatman <kchatman@gmail.com>
2020-01-03 16:52:46 -0800

    added karatsuba, needs optimization

--- 43 Multiply Strings/main.py
+++ 43 Multiply Strings/main.py
@@ -16,27 +16,90 @@
 # Both num1 and num2 do not contain any leading zero, except the number 0 itself.
 # You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
+import time
+import random
+
 class Solution:
+    threshold = 10
+
     def multiply(self, num1: str, num2: str) -> str:
-        threshold = 10
         num1 = self.trim_int_list(self.string_to_int_array(num1))
         num2 = self.trim_int_list(self.string_to_int_array(num2))
-        if len(num1) < threshold or len(num2) < threshold:
+        if len(num1) < self.threshold or len(num2) < self.threshold:
             result = self.long_mult(num1, num2)
         else:
             result = self.k_mult(num1, num2)
         return self.int_array_to_string(result)
 
+    def k_mult(self, num1, num2):
+        """ 
+        karatsuba algorithm
+        takes int lists, returns int list
+        based on https://en.wikipedia.org/wiki/Karatsuba_algorithm#Pseudocode
+        """ 
+        min_len = min(len(num1), len(num2))
+        if min_len < self.threshold:
+            return self.long_mult(num1, num2)
+        shift = min_len // 2
+        
+        high1, low1 = self.split_at_shift(num1, shift)
+        high2, low2 = self.split_at_shift(num2, shift)
 
-    def add_to(self, base, to_add, shift):
+        # high1 = num1[:div]
+        # low1 = num1[div:]
+        # high2 = num2[:div]
+        # low2 = num2[div:]
+
+        z0 = self.k_mult(low1, low2) 
+        low1 = self.add_to(low1, high1, 0) 
+        low2 = self.add_to(low2, high2, 0)
+        z1 = self.k_mult(low1, low2)
+        z2 = self.k_mult(high1, high2)
+
+        result = [0]
+        result = self.add_to(result, z2, 2 * shift)
+        result = self.add_to(result, z0, 0)
+        result = self.add_to(result, z1, shift)
+        result = self.sub_from(result, z2, shift)
+        result = self.sub_from(result, z0, shift)
+
+        return self.trim_int_list(result)
+        
+
+    def split_at_shift(self, num, shift):
+        div = len(num) - shift
+        high = num[:div]
+        low = num[div:]
+        return high, low
+
+
+    def add_to(self, base, to_add, shift): 
+        # todo fix bug: can give 10 as first digit
+        if len(base) < len(to_add) + 1 + shift:
+            result = [0] * (len(to_add) + 1 + shift - len(base)) + base
+        else:
+            result = [0] + base
         # add all
         for i in range(len(to_add)):
-            base[len(base) - 1 - shift - i] += to_add[len(to_add) - 1 - i]
+            result[len(result) - 1 - shift - i] += to_add[len(to_add) - 1 - i]
+        # do carries
+        for i in range(len(result) - 1):
+            if result[len(result) - 1 - i] > 9:
+                result[len(result) - 1 - i] -= 10
+                result[len(result) - 1 - i - 1] += 1
+        return self.trim_int_list(result)
+
+
+    def sub_from(self, base, to_sub, shift):
+        # add all
+        for i in range(len(to_sub)):
+            base[len(base) - 1 - shift - i] -= to_sub[len(to_sub) - 1 - i] # got index out of range error?
         # do carries
         for i in range(len(base) - 1):
-            if base[len(base) - 1 - i] > 9:
-                base[len(base) - 1 - i] -= 10
-                base[len(base) - 1 - i - 1] += 1
+            if base[len(base) - 1 - i] < 0:
+                base[len(base) - 1 - i] += 10
+                base[len(base) - 1 - i - 1] -= 1
+        return base
         
 
     def long_mult(self, num1, num2):
@@ -54,7 +117,7 @@ class Solution:
             if carry != 0:
                 product[len(product) - i - len(num2) - 1] += carry
                 carry = 0
-        return product
+        return self.trim_int_list(product)
         
     def string_to_int_array(self, string):
         return [int(i) for i in string]
@@ -84,9 +147,36 @@ class Solution:
 
 def main():
     """starts_here"""
-    sol = Solution()
+    length = 200
+    n_numbers = 100
+    numbers = list()
 
-    print(sol.multiply("10", "5"))
+    for i in range(n_numbers):
+        to_add = list()
+        for j in range(length):
+            if j == 0:
+                to_add.append(random.randint(1, 9))
+            else:
+                to_add.append(random.randint(0, 9))
+        numbers.append(to_add)
+
+    sol_long = Solution()
+    sol_long.threshold = 1000
+    sol_kara = Solution()
+    sol_kara.threshold = 2
+    time_long = 0
+    time_k = 0
+    for i in range(n_numbers):
+        for j in range(i, n_numbers):
+            start = time.time()
+            sol_long.long_mult(numbers[i], numbers[j])
+            time_long += time.time() - start
+            start = time.time()
+            sol_kara.k_mult(numbers[i], numbers[j])
+            time_k += time.time() - start
+    print("long: " + str(time_long) + ", kara: " + str(time_k))
+            
+        
 
 if __name__ == '__main__':
     """starts here"""
--- 43 Multiply Strings/test_main.py
+++ 43 Multiply Strings/test_main.py
@@ -3,10 +3,90 @@
 import main
 
 import unittest
+import random
 
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
+
+    def test_stress(self):
+        enabled = True
+
+        if not enabled:
+            return
+
+        # get two lists of string numbers
+        max_len = 20
+        n_numbers = 100
+        numbers = list()
+
+        for i in range(n_numbers):
+            to_add = list()
+            length = random.randint(0, max_len)
+            for j in range(length):
+                if j == 0:
+                    to_add.append(random.randint(1, 9))
+                else:
+                    to_add.append(random.randint(0, 9))
+            numbers.append(to_add)
+
+        sol_long = main.Solution()
+        sol_long.threshold = 1000
+        sol_kara = main.Solution()
+        sol_kara.threshold = 2
+
+        for i in range(n_numbers):
+            for j in range(i, n_numbers):
+                self.assertEqual( sol_long.long_mult(numbers[i], numbers[j]), 
+                                  sol_kara.k_mult(numbers[i], numbers[j]),
+                                  str(numbers[i]) + ", " + str(numbers[j]) )
+        
+
+
+
+    def test_split_at_shift(self):
+        sol = main.Solution()
+
+        num = [1, 2, 3]
+        shift = 1
+        high_expected = [1, 2]
+        low_expected = [3]
+        high_result, low_result = sol.split_at_shift(num, shift)
+        self.assertEqual(high_expected, high_result)
+        self.assertEqual(low_expected, low_result)
+
+        num = [1, 2, 3]
+        shift = 2
+        high_expected = [1]
+        low_expected = [2, 3]
+        high_result, low_result = sol.split_at_shift(num, shift)
+        self.assertEqual(high_expected, high_result)
+        self.assertEqual(low_expected, low_result)
+
+    def test_add_to(self):
+        sol = main.Solution()
+
+        base = list([1, 0, 0])
+        to_add = [5]
+        shift = 1
+        expected = list([1, 5, 0])
+        result = sol.add_to(base, to_add, shift)
+        self.assertEqual(expected, result)
+
+        base = list([1, 9, 9])
+        to_add = [2]
+        shift = 0
+        expected = list([2, 0, 1])
+        result = sol.add_to(base, to_add, shift)
+        self.assertEqual(expected, result)
+
+        base = list([1, 2, 3, 4])
+        to_add = [3, 2]
+        shift = 1
+        expected = list([1, 5, 5, 4])
+        result = sol.add_to(base, to_add, shift)
+        self.assertEqual(expected, result)
+
 
     def test_multiply(self):
         sol = main.Solution()
@@ -42,31 +122,53 @@ class BasicTestSuite(unittest.TestCase):
         self.assertEqual(expected, result)
 
 
-    def test_add_to(self):
+    def test_k_mult(self):
         sol = main.Solution()
+        sol.threshold = 2 # take all the way down to single digit
 
-        base = list([1, 0, 0])
-        to_add = [5]
-        shift = 1
-        expected = list([1, 5, 0])
-        sol.add_to(base, to_add, shift)
-        self.assertEqual(expected, base)
+        num1 = [4, 7, 5, 4] 
+        num2 = [2, 1, 0, 4, 8, 0, 1, 5, 8, 3, 2, 5, 3, 3, 0, 3, 4, 6, 5, 7]
+        expected = [1, 0, 0, 0, 6, 2, 2, 6, 7, 2, 6, 7, 8, 6, 2, 0, 4, 6, 7, 5, 9, 3, 7, 8]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
 
-        base = list([1, 9, 9])
-        to_add = [2]
-        shift = 0
-        expected = list([2, 0, 1])
-        sol.add_to(base, to_add, shift)
-        self.assertEqual(expected, base)
+        num1 = [1, 0]
+        num2 = [5]
+        expected = [5, 0]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
 
-        base = list([1, 2, 3, 4])
-        to_add = [3, 2]
-        shift = 1
-        expected = list([1, 5, 5, 4])
-        sol.add_to(base, to_add, shift)
-        self.assertEqual(expected, base)
+        num1 = [0]
+        num2 = [4, 5, 6]
+        expected = [0]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
 
+        num1 = [1, 2]
+        num2 = [1, 0]
+        expected = [1, 2, 0]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
 
+        num1 = [1, 2, 3]
+        num2 = [4, 5, 6]
+        expected = [5, 6, 0, 8, 8]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
+
+        num1 = [4, 5, 6, 8]
+        num2 = [4, 5, 6]
+        expected = [2, 0, 8, 3, 0, 0, 8]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
+
+        num1 = [1, 0, 0, 0]
+        num2 = [1, 0, 2]
+        expected = [1, 0, 2, 0, 0, 0]
+        result = sol.k_mult(num1, num2)
+        self.assertEqual(expected, result)
+
+    
     def test_string_to_int_array(self):
         sol = main.Solution()
 
@@ -102,10 +204,11 @@ class BasicTestSuite(unittest.TestCase):
 
     def test_long_mult(self):
         sol = main.Solution()
+        sol.threshold = 200
 
         num1 = list((1, 2, 3))
         num2 = list((3, 5)) 
-        expected = list((0, 4, 3, 0, 5))
+        expected = list((4, 3, 0, 5))
         result = sol.long_mult(num1, num2)
         self.assertEqual(expected, result)
 
