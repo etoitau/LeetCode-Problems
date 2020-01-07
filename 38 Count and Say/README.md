87c7e505d7bcbc93bfc9afccdfd9ba85a53eec72
Kyle Chatman <kchatman@gmail.com>
2020-01-07 11:28:51 -0800

    update 38 for code sample

---
+++ 38 Count and Say/README.md
@@ -0,0 +1,29 @@
+# Code Sample
+Provided for everlaw job application by [Kyle Chatman](http://www.kchatman.com)
+
+## Introduction
+Please note I have about 20 repositories shared publicly on [GitHub](https://github.com/etoitau)
+so if this isn't what you are looking for you can probably find it there. You can also find
+descriptions, video demos, code, and links to live versions of some of my projects at 
+[cs.kchatman.com](http://cs.kchatman.com)
+
+I picked this sample because:
+- It is concise and has a quickly understood problem to solve and solution
+- I thought the numerical sequence central to the problem was amusing
+- My solution uses some outside the box thinking to perform better than any other
+
+## Description Of Approach
+I noticed that the spec only required a solution to be able to provide one of the first 30
+terms in the sequence. And while the sequence does grow something like exponentially, 
+the 30th term is not prohibitively large. So I just submitted a method which had the first
+30 terms hard-coded and returned whichever one was required.
+
+That said, I did still have to write code to generate the first 30 terms, and went a bit 
+farther to format it into code that I could paste into my solution method, so hopefully you 
+agree that it's more cheeky than cheating. 
+
+## Problem Taken From
+https://leetcode.com/problems/count-and-say/
+
+## Source at
+https://github.com/etoitau/LeetCode-Problems/tree/master/38%20Count%20and%20Say
--- 38 Count and Say/src/test/java/com/etoitau/leetcode/countandsay/MainTest.java
+++ 38 Count and Say/src/test/java/com/etoitau/leetcode/countandsay/MainTest.java
@@ -10,6 +10,90 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
+    void get() {
+        CountSaySeries css;
+        int input;
+        String expected, result;
+
+        css = new CountSaySeries();
+        input = 3;
+        expected = "21";
+        result = css.get(input);
+        assertEquals(expected, result);
+
+        input = 6;
+        expected = "312211";
+        result = css.get(input);
+        assertEquals(expected, result);
+
+        input = 4;
+        expected = "1211";
+        result = css.get(input);
+        assertEquals(expected, result);
+
+        input = 1;
+        expected = "1";
+        result = css.get(input);
+        assertEquals(expected, result);
+
+        input = 0;
+        expected = null;
+        result = css.get(input);
+        assertEquals(expected, result);
+
+        input = -3;
+        expected = null;
+        result = css.get(input);
+        assertEquals(expected, result);
+
+        css = new CountSaySeries(10);
+        input = 4;
+        expected = "1211";
+        result = css.get(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void toString1() {
+        CountSaySeries css = new CountSaySeries();
+
+        String expected, result;
+
+        expected = "1. 1";
+        result = css.toString();
+        assertEquals(expected, result);
+
+        css.get(3);
+        expected = "1. 1\n" +
+                "2. 11\n" +
+                "3. 21";
+        result = css.toString();
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void toCode() {
+        CountSaySeries css = new CountSaySeries();
+
+        String expected, result;
+
+        expected = "String[] sequence = new String[] {\n" +
+                "    \"1\"\n" +
+                "};";
+        result = css.toCode();
+        assertEquals(expected, result);
+
+        css.get(3);
+        expected = "String[] sequence = new String[] {\n" +
+                "    \"1\",\n" +
+                "    \"11\",\n" +
+                "    \"21\"\n" +
+                "};";
+        result = css.toCode();
+        assertEquals(expected, result);
+    }
+
+    @Test
     void solutionTest() {
         Solution sol = new Solution();
         int n;
@@ -36,12 +120,10 @@ class MainTest {
         assertEquals(expected, result);
 
         n = 30;
-        expected = "";
         try {
             sol.countAndSay(n);
         } catch (Exception e) {
             fail();
         }
-
     }
 }
